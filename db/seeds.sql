1137039483b79609bf4a3be5885495bcd322decc
Michael Simmons <simmons.md94@gmail.com>
2019-08-15 19:06:11 -0400

    first commit - sql showing errors

---
+++ db/seeds.sql
@@ -0,0 +1,22 @@
+
+USE `burgers_db`;
+
+
+INSERT INTO `burgers` (
+burger_name,
+devoured,
+date
+) VALUES
+('Big Mac', false, CURRENT_TIMESTAMP);
+INSERT INTO `burgers` (
+burger_name,
+devoured,
+date
+) VALUES
+('Beyond', false, CURRENT_TIMESTAMP);
+INSERT INTO `burgers` (
+burger_name,
+devoured,
+date
+) VALUES
+('Pimento', false, CURRENT_TIMESTAMP);
